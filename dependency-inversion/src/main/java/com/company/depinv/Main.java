f5b01a307e0910f8dd9dcea5b9507a2a218a5944
gustavooarantes <profissional.gustavoa@gmail.com>
2025-09-13 16:59:23 -0300

    Final refactoring completed - no more dependency on concrete-volatile components

--- dependency-inversion/src/main/java/com/company/depinv/Main.java
+++ dependency-inversion/src/main/java/com/company/depinv/Main.java
@@ -1,6 +1,8 @@
 package com.company.depinv;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Main {
 
@@ -8,7 +10,10 @@ public class Main {
 		
 		Message msg = new Message("This is a message again");
 		MessagePrinter printer = new MessagePrinter();
-		printer.writeMessage(msg, "test_msg.txt");
+
+		try (PrintWriter writer = new PrintWriter(new FileWriter("test_msg.txt"))) {
+			printer.writeMessage(msg, new JSONFormatter(), writer);
+		}
 	}
 
 }
